b23d5089104216ee02e9819ac880c7ef60ebb860
vekvek <65372577+bayvekvek@users.noreply.github.com>
2020-05-15 02:00:37 +0300

    sametfor men
    qqqfqfqfqefw

--- New Unity Project/Assets/Character.cs
+++ New Unity Project/Assets/Character.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(samet for men)
+    { samet for men
         
     }
 
